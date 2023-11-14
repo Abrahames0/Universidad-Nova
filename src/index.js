b75d34c0f2906f7d7705a5fc8f2d941c27629818
Abraham Espinoza <abraham.espinoza@optimen.com.mx>
2023-11-13 18:01:08 -0600

    Librerias y paquetes que se usaran

--- src/index.js
+++ src/index.js
@@ -1,15 +1,68 @@
+//Importaciones de React
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+//Importaciones AWS
+import awsconfig from './aws-exports';
+import { translations } from '@aws-amplify/ui-react';
+import { Amplify, Auth, AuthModeStrategyType, I18n } from 'aws-amplify';
+//Importaciones Estilo
+import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ThemeProvider, createTheme } from "@mui/material";
+//Importaciones de paginas
+import Apps from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React>
-    <App />
-  </React>
+I18n.putVocabularies(translations);
+I18n.setLanguage('es');
+
+I18n.putVocabularies({
+  es: {
+    'Enter your Email': 'Ingresa tu correo electrónico',
+    'Enter your Password': 'Ingresa tu contraseña',
+    'Please confirm your Password': 'Confirma tu contraseña',
+    'Password must have at least 8 characters': 'La contraseña debe tener al menos 8 caracteres',
+    'Password must have numbers': 'La contraseña debe tener números',
+    'Password must have lower case letters': 'La contraseña debe tener letras minúsculas',
+    'Password must have special characters': 'La contraseña debe tener caracteres especiales',
+    'Password must have upper case letters': 'La contraseña debe tener letras mayúsculas',
+    'User does not exist.': 'El usuario no existe',
+    'Network error': 'Error de red'
+  }
+});
+
+Amplify.configure({ ...awsconfig, DataStore: { authModeStrategyType: AuthModeStrategyType.MULTI_AUTH } });
+Auth.configure(awsconfig);
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#5d4fc6',
+    },
+  },
+
+  typography: {
+    button: {
+      textTransform: 'none',
+    },
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Apps />,
+  },
+]);
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <App />
 );
-
-
-reportWebVitals();
