e20af75a21e30c36b561164ddea0c394165cbc2f
Abraham Espinoza <abraham.espinoza@optimen.com.mx>
2023-11-21 00:33:20 -0600

    formulario registro modificacion index

--- src/index.js
+++ src/index.js
@@ -13,10 +13,40 @@ import LoginEstudiantes from './pages/perfil-Estudiante/LoginEstudiantes';
 import Inicio from './pages/inicio-Principal/Inicio';
 import { StepperRegistro } from './components/RegistroEstudiante/StepperRegistroEstudiante';
 
+I18n.putVocabularies(translations);
+I18n.setLanguage('es');
+
+I18n.putVocabularies({
+  es: {
+    'Enter your Email': 'Ingresa tu correo electrónico',
+    'Enter your Password': 'Ingresa tu contraseña',
+    'Please confirm your Password': 'Confirma tu contraseña',
+    'Password must have at least 8 characters': 'La contraseña debe tener al menos 8 caracteres',
+    'Password must have numbers': 'La contraseña debe tener números',
+    'Password must have lower case letters': 'La contraseña debe tener letras minúsculas',
+    'Password must have special characters': 'La contraseña debe tener caracteres especiales',
+    'Password must have upper case letters': 'La contraseña debe tener letras mayúsculas',
+    'User does not exist.': 'El usuario no existe',
+    'Network error': 'Error de red'
+  }
+});
 
 Amplify.configure({ ...awsconfig, DataStore: { authModeStrategyType: AuthModeStrategyType.MULTI_AUTH } });
 Auth.configure(awsconfig);
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#5d4fc6',
+    },
+  },
+
+  typography: {
+    button: {
+      textTransform: 'none',
+    },
+  },
+});
 
 
 const router = createBrowserRouter([
@@ -36,7 +66,9 @@ const router = createBrowserRouter([
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
       <RouterProvider router={router} />
+    </ThemeProvider>
   );
 }
 
