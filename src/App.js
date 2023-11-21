06bfa89ce1cffe4649a657b8d06c40c6bd988d7d
FernandoASAS <fernandoteloso@gmail.com>
2023-11-21 00:17:26 -0600

    algo aca

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
                   Misión
                 </Typography>
                 <Typography variant="body1" align="center">
-                  Descripción de la misión...
+                Somos una institución de Educación Superior Tecnológica comprometida en la formación integral de profesionistas líderes en los niveles de Técnico Superior Universitario y Licenciatura con programas educativos de excelencia, contribuyendo así, al desarrollo económico y social del estado y la región.
                 </Typography>
               </CardContent>
             </Card>
@@ -79,7 +79,7 @@ function App() {
                   Visión
                 </Typography>
                 <Typography variant="body1" align="center">
-                  Descripción de la visión...
+                En el 2026 la Universidad Tecnológica del Norte de Guanajuato será una institución de educación superior reconocida nacional e internacionalmente por formar egresados competitivos que se integran exitosamente al ámbito laboral, en los que resaltará su formación humana e integral; se contará con programas educativos de excelencia, pertinentes e incluyentes: en los que se promueve la investigación aplicada e innovación tecnológica a través de sus cuerpos académicos; se tendrá una fuerte vinculación con todos los sectores y se impulsará el emprendimiento y la competitividad, contribuyendo así a la transformación de la educación en Guanajuato. Se consolidará el enfoque hacía la igualdad y no discriminación.
                 </Typography>
               </CardContent>
             </Card>
@@ -92,7 +92,25 @@ function App() {
                   Valores
                 </Typography>
                 <Typography variant="body1" align="center">
-                  Descripción de los valores...
+                Para la UTNG los valores son el marco del comportamiento que deben tener sus integrantes, estos se determinaron en base a la razón de ser; al propósito de creación; y a la proyección en el futuro (visión) de la institución quedando redactados y descritos de la manera siguiente:
+
+                Rendición de Cuentas:
+
+                Transparencia:
+
+                Honestidad:
+
+
+                Responsabilidad:
+
+                Respeto:
+
+                Trabajo en equipo:.
+
+                Comunicación:
+
+                Paz:
+
                 </Typography>
               </CardContent>
             </Card>
